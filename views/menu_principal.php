bc2973a211c912baa09d7d0b683ec0d86e14d4e2
starkoi <sonya.b@hotmail.ca>
2017-04-22 14:44:26 -0400

    change h a 1 et f a 0

--- views/menu_principal.php
+++ views/menu_principal.php
@@ -2,20 +2,20 @@
 ?>
 <nav>
     <ul id="menu-deroulant">
-        <li>Homme
+        <li><a href="produits.php?sexe=1">Homme</a>
            <ul class="sous_menu">
-                <li><a href="produits.php?cat_id=1&sexe=0">Bottes</a></li>
-                <li><a href="produits.php?cat_id=4&sexe=0">Chaussures</a></li>
-                <li><a href="produits.php?cat_id=2&sexe=0">Sandales</a></li>
-                <li><a href="produits.php?cat_id=3&sexe=0">Sport</a></li>
-            </ul>
-        </li>
-        <li>Femme
-            <ul class="sous_menu">
                 <li><a href="produits.php?cat_id=1&sexe=1">Bottes</a></li>
                 <li><a href="produits.php?cat_id=4&sexe=1">Chaussures</a></li>
                 <li><a href="produits.php?cat_id=2&sexe=1">Sandales</a></li>
                 <li><a href="produits.php?cat_id=3&sexe=1">Sport</a></li>
+            </ul>
+        </li>
+        <li><a href="produits.php?sexe=0">Femme</a>
+            <ul class="sous_menu">
+                <li><a href="produits.php?cat_id=1&sexe=0">Bottes</a></li>
+                <li><a href="produits.php?cat_id=4&sexe=0">Chaussures</a></li>
+                <li><a href="produits.php?cat_id=2&sexe=0">Sandales</a></li>
+                <li><a href="produits.php?cat_id=3&sexe=0">Sport</a></li>
             </ul>
         </li>
     </ul>
