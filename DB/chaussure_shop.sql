152968ac4fda1fc4aae3408ffd93179c134a5b4e
cyiphum <carolina.yiphum@gmail.com>
2017-04-22 12:14:38 -0400

    "changement du nom du dossier categorie sport."

--- DB/chaussure_shop.sql
+++ DB/chaussure_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 21 Avril 2017 à 23:09
+-- Généré le :  Ven 21 Avril 2017 à 23:59
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
