dab73ad8c4137f9e0a8bd1972754dd4c1eb71d17
starkoi <sonya.b@hotmail.ca>
2017-04-22 14:43:17 -0400

    add lien vers function triage

--- produits.php
+++ produits.php
@@ -1,20 +1,22 @@
 <?php
 require_once('DB/connection.php');
-var_dump($_GET);
 require_once ('common/defines.php');
 require_once ('views/page_top.php');
 require_once('views/menu_panier.php');
-$chaussures = get_chaussures_avec_cats();
+
+$cat_id = (isset($_GET['cat_id'])? $_GET['cat_id']: null);
+$sexe= (isset($_GET['sexe'])? $_GET['sexe']: null);
+
+$chaussures = get_chaussures_avec_cats($cat_id, $sexe);
 
 ?>
 <main>
-
     <ul>
         <?php
         foreach ($chaussures as $id => $c){
             //var_dump($chaussures);
             $img = '<img src="'.$c["full_image_path"].'" alt= "image de chaussure"/>';
-            var_dump($img);
+           // var_dump($img);
 
             echo '<li class="chaussures">'.$c["marque"] .$c["nom"] . $img. $c["description"]. $c["prix"]. $c["pointure"].'</li>';
         }
