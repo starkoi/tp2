e1bac2349b5111d053cae294b6a6f966722ecadc
cyiphum <carolina.yiphum@gmail.com>
2017-04-22 12:12:44 -0400

    "affichage de tous les produits hommes et femmes."

--- produits.php
+++ produits.php
@@ -3,8 +3,19 @@ require_once('DB/connection.php');
 var_dump($_GET);
 require_once ('common/defines.php');
 require_once ('views/page_top.php');
+$chaussures = get_chaussures_avec_cats();
 ?>
 <main>
+    <ul>
+        <?php
+        foreach ($chaussures as $id => $c){
+            //var_dump($chaussures);
+            $img = '<img src="'.$c["full_image_path"].'" alt= "image de chaussure"/>';
+            var_dump($img);
 
+            echo '<li class="chaussures">'.$c["marque"] .$c["nom"] . $img. $c["description"]. $c["prix"]. $c["pointure"].'</li>';
+        }
+        ?>
+    </ul>
 </main>
 <?php require_once ('views/page_bottom.php') ?>
