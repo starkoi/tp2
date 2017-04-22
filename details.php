d372cd7620e15cac12f92ae0690081577555dd48
cyiphum <carolina.yiphum@gmail.com>
2017-04-22 12:18:32 -0400

    "affichage du nom de produit sans filtrage dans la page details"

--- details.php
+++ details.php
@@ -1,8 +1,17 @@
 <?php
 require_once ('common/defines.php');
 require_once ('views/page_top.php');
+require_once ('DB/connection.php');
+$chaussures = get_chaussures();
 ?>
 <main>
+<ul>
+    <?php
+    foreach ($chaussures as $id => $c){
+        echo '<li class="chaussures">'.$c["name"].'</li>';
+    }
+    ?>
 
+</ul>
 </main>
 <?php require_once ('views/page_bottom.php') ?>
