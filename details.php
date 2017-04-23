73bdee18c160c42015e1ea388423477227cc74f9
cyiphum <carolina.yiphum@gmail.com>
2017-04-23 11:23:20 -0400

    "affichage du produit dans la page detail avec tout la description complet du produit"

--- details.php
+++ details.php
@@ -2,16 +2,33 @@
 require_once ('common/defines.php');
 require_once ('views/page_top.php');
 require_once ('DB/connection.php');
-$chaussures = get_chaussures();
-?>
-<main>
-<ul>
-    <?php
-    foreach ($chaussures as $id => $c){
-        echo '<li class="chaussures">'.$c["name"].'</li>';
-    }
-    ?>
+if (array_key_exists('produit_id',$_GET)&& ($_GET['produit_id'] !==null)) {
+    $produit_id = $_GET['produit_id'];
 
-</ul>
-</main>
+    $chaussures = get_chaussures_avec_cats(null, null, $produit_id);
+    $chaussure = $chaussures[$produit_id];
+
+
+    ?>
+    <main>
+        <?php
+        $img = '<img src="'.$chaussure["full_image_path"].'" alt= "image de chaussure"/>';
+
+        echo    '<div class="chaussures">'.
+            ucfirst($chaussure["marque"]) . ' ' . ucfirst($chaussure["nom"]) .
+            $img.
+            $chaussure["description"].
+            $chaussure["prix"]. ' ' .
+            $chaussure["pointure"].
+            "<a href='?op=ajouter&itemid=".$produit_id."&produit_id=".$produit_id."'> Ajouter</a>".
+            '</div>';
+        ?>
+    </main>
+<?php
+}else {
+    header('Location: index.php');
+}
+
+?>
+
 <?php require_once ('views/page_bottom.php') ?>
