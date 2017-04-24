3116486d23155cd2824f21880419fd305995b3f0
starkoi <sonya.b@hotmail.ca>
2017-04-24 12:33:20 -0400

    enleve count

--- views/header.php
+++ views/header.php
@@ -6,7 +6,6 @@
     <div id="logo" class="img_header"><a href="index.php"></a></div>
     <?php require_once ('views/menu_principal.php')?>
     <div id="menu-secondaire">
-        <div id="count" class="img_header"><?php require_once('views/panier_views.php')?></div>
         <div id="panier" class="img_header"><a href="panier.php"><div><?php echo $total; ?></div></a></div>
         <div id="user" class="img_header"><a href="conexion.php"></a></div>
     </div>
