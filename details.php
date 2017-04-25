2a760cd124ee4502e648be7fca97bf8443886e20
cyiphum <carolina.yiphum@gmail.com>
2017-04-24 22:59:58 -0400

    "reorganisation du code "

--- details.php
+++ details.php
@@ -1,9 +1,15 @@
 <?php
 require_once('common/defines.php');
-require_once('views/page_top.php');
+
 
 if (array_key_exists('produit_id', $_GET) && ($_GET['produit_id'] !== null)) {
+    require_once('views/page_top.php');
+    $cat_id = null;
     $produit_id = $_GET['produit_id'];
+
+    if  (array_key_exists('cat_id',$_GET) && categorie_exists($_GET['cat_id'])) {
+        $cat_id = $_GET['cat_id'];
+    }
 
     $chaussures = get_chaussures_avec_cats(null, null, $produit_id);
     $chaussure = $chaussures[$produit_id];
@@ -16,10 +22,12 @@ if (array_key_exists('produit_id', $_GET) && ($_GET['produit_id'] !== null)) {
 
     echo '<div class="chaussures">' .
     '<p>' . ucfirst($chaussure["marque"]) . ' ' . ucfirst($chaussure["nom"]) . '</p>'. $img  . $chaussure["description"] .  '<p class="prix_chaussure">' . $chaussure["prix"] . '</p>' .
-            "<a class='ajouter_panier' href=' ? op = ajouter & itemid = ".$produit_id." & produit_id = ".$produit_id."'> Ajouter</a>".
+            "<a class='ajouter_panier' href=' ? op = ajouter & itemid = ".$produit_id." & produit_id = ".$produit_id.'&sexe='.$chaussure['sexe'].((!is_null($cat_id))?"&cat_id=".$cat_id : "")."'> Ajouter</a>" .
             ' </div > ';
         ?>
     </main>
+    <?php require_once ('views/page_bottom.php') ?>
+
 <?php
 }else {
     header('Location:index.php');
@@ -27,4 +35,4 @@ if (array_key_exists('produit_id', $_GET) && ($_GET['produit_id'] !== null)) {
 
 ?>
 
-<?php require_once ('views /page_bottom.php') ?>
+
