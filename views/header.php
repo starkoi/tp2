84b12d29a5958a70f319fb7341261787f6ee4c4d
edetchev <elorri.detcheverry@gmail.com>
2017-04-25 08:26:14 -0400

    correction du login (petite phrase dans le header) + ajout d'un pattern sur le bordy pour faire un bg

--- views/header.php
+++ views/header.php
@@ -1,13 +1,27 @@
 <?php
-    $total = 0;
-    foreach ($panier as $qte) { $total += $qte; }
+$total = 0;
+foreach ($panier as $qte) {
+    $total += $qte;
+}
+require_once('_login.php')
+
 ?>
 <header>
     <div id="logo" class="img_header"><a href="index.php"></a></div>
-    <?php require_once ('views/menu_principal.php')?>
+    <?php require_once('views/menu_principal.php') ?>
     <div id="menu-secondaire">
-        <div id="panier" class="img_header"><a href="panier.php"><div><?php echo $total; ?></div></a></div>
+        <div id="panier" class="img_header"><a href="panier.php">
+                <div><?php echo $total; ?></div>
+            </a></div>
         <div id="user" class="img_header"><a href="conexion.php"></a></div>
-        <div id="username"> <span><?= 'Bonjour ' . $_SESSION['username'] ?></span></div>
+        <div id="username"><span><?php
+
+                if (user_is_logged()) {
+                    echo 'Bonjour ' . $_SESSION['username'];
+                } else {
+                    echo 'Bonjour, veuillez vous connecter.';
+                } ?>
+
+            </span></div>
     </div>
 </header>
